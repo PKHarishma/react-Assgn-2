76a318972f12b2f7c74f03cf1e2c2e7adaba5877
PKHarishma <pkharishma73@gmail.com>
2024-08-24 07:32:18 +0530

    react assignment 2 added

---
+++ script.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client" ;
+const para=<h1>hai i am here</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(para);
